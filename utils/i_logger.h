37e5f7137c7b91727cb89f725641483a1bad27fd
R4nd0m6uy <r4nd0m6uy@r4nd0m6uy.ch>
2021-01-19 17:46:07 +0100

    Add logger instance holding a console logger
    This makes easier to change the logger implementation in case we want to
    use other mean of logging such as syslog

---
+++ utils/i_logger.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2021 Medusalix
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#ifndef XOW_I_LOGGER_H_
+#define XOW_I_LOGGER_H_
+
+#include <ostream>
+
+namespace Log
+{
+
+enum class Level {
+  LOG_DEBUG,
+  LOG_INFO,
+  LOG_ERROR
+};
+
+class ILogger {
+public:
+    ILogger() = default;
+    ~ILogger() = default;
+
+    virtual void init() = 0;
+    virtual void sinkLog(Level level, const std::string& message) = 0;
+};
+
+inline std::ostream& operator<<(std::ostream& os, const Level& level) {
+    switch(level) {
+        case Level::LOG_DEBUG:
+          os << "DEBUG";
+          break;
+        case Level::LOG_INFO:
+          os << "INFO";
+          break;
+        case Level::LOG_ERROR:
+          os << "ERROR";
+          break;
+    }
+
+    return os;
+}
+
+}
+#endif
---
+++ utils/logger_console.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2021 Medusalix
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#include <sstream>
+#include <iomanip>
+
+#include "logger_console.h"
+
+namespace Log
+{
+
+void LoggerConsole::init() {
+    setlinebuf(stdout);
+    setlinebuf(stderr);
+}
+
+void LoggerConsole::sinkLog(Level level, const std::string& message) {
+    FILE* outstream = stdout;
+
+    if(level == Level::LOG_ERROR) {
+        outstream = stderr;
+    }
+    std::fputs(formatLog(level, message).c_str(), outstream);
+}
+
+std::string LoggerConsole::formatLog(Level level, const std::string& message) {
+    std::ostringstream stream;
+    std::time_t time = std::time(nullptr);
+    std::tm localTime = {};
+
+    // Add local time to output if available
+    if (localtime_r(&time, &localTime))
+    {
+        stream << std::put_time(&localTime, "%F %T") << " ";
+    }
+
+    stream << std::left << std::setw(5);
+    stream << level << " - ";
+    stream << message << std::endl;
+
+    return stream.str();
+}
+
+}
